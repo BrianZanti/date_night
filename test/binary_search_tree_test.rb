5dc3abfa8f889ed7413fff9fd6bfef2e01e2e53b
Brian Zanti <brianzanti@gmail.com>
2018-03-22 13:40:05 -0600

    changes test to implement empty node feature

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -1,6 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/binary_search_tree'
+require 'pry'
 
 class BinarySearchTreeTest < Minitest::Test
 
@@ -21,13 +22,19 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal "name", @bst.root.name
   end
 
+  def test_inserting_root_creates_empty_children
+    @bst.insert(0, "name")
+    assert @bst.root.left.empty?
+    assert @bst.root.right.empty?
+  end
+
   def test_can_insert_muliple
     @bst.insert(5, "5")
     @bst.insert(1, "1")
     @bst.insert(6, "6")
     @bst.insert(3, "3")
     @bst.insert(2, "2")
-
+    # binding.pry
     assert_equal 5, @bst.root.score
     assert_equal 1, @bst.root.left.score
     assert_equal 6, @bst.root.right.score
