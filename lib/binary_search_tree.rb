37cf86d8adbcf4f2b1af866dab57da9c370dfd74
Brian Zanti <brianzanti@gmail.com>
2018-03-22 13:40:33 -0600

    implements insert with empty node feature

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -10,17 +10,29 @@ class BinarySearchTree
   def insert(score, name)
     if @root == nil
       @root = Node.new(score,name)
+      @root.left = Node.new(nil,nil,true)
+      @root.right = Node.new(nil,nil,true)
     else
-      new_node = Node.new(score, name)
-      insert_recursive(@root, new_node)
+      insert_recursive(@root, score, name)
     end
   end
 
-  def insert_recursive(current_node, new_node)
-    if new_node.score < current_node.score
-      insert_left(current_node, new_node)
+  def insert_recursive(current_node, score, name)
+    if current_node.empty?
+      current_node.fill(score,name)
+      current_node.left = Node.new(nil,nil,true)
+      current_node.right = Node.new(nil,nil,true)
     else
-      insert_right(current_node, new_node)
+      next_node = get_next_node(current_node, score)
+      insert_recursive(next_node, score, name)
+    end
+  end
+
+  def get_next_node(current_node, score)
+    if score < current_node.score
+      current_node.left
+    else
+      current_node.right
     end
   end
 
