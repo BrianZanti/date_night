90bbf665e818e48b3959f41cd8ed4f2c6e4c6a9b
Brian Zanti <brianzanti@gmail.com>
2018-03-22 13:47:16 -0600

    removes old methods

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -29,26 +29,7 @@ class BinarySearchTree
   end
 
   def get_next_node(current_node, score)
-    if score < current_node.score
-      current_node.left
-    else
-      current_node.right
-    end
-  end
-
-  def insert_left(current_node, new_node)
-    if current_node.left == nil
-      current_node.left = new_node
-    else
-      insert_recursive(current_node.left, new_node)
-    end
-  end
-
-  def insert_right(current_node, new_node)
-    if current_node.right == nil
-      current_node.right = new_node
-    else
-      insert_recursive(current_node.right, new_node)
-    end
+    return current_node.left if score < current_node.score
+    current_node.right
   end
 end
