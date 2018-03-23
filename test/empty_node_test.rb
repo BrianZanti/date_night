9a86cbaa7fe7d2c71031549583b74dcd9f970716
Brian Zanti <brianzanti@gmail.com>
2018-03-22 20:49:51 -0600

    removes EmptyNode

--- test/empty_node_test.rb
+++
@@ -1,9 +0,0 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/empty_node'
-
-class EmptyNodeTest < Minitest::Test
-  def test_it_exists
-    assert_instance_of EmptyNode, EmptyNode.new
-  end
-end
