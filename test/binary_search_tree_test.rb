71f58e471157d0c40af76593e0ae7735bc31523d
Brian Zanti <brianzanti@gmail.com>
2018-05-10 15:03:47 -0600

    adds test for min

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -78,6 +78,13 @@ class BinarySearchTreeTest < Minitest::Test
     assert_equal expected, @bst.max
   end
 
+  def test_min
+    expected = {
+      "1" => 1
+    }
+    assert_equal expected, @bst.min
+  end
+
   def test_count
     assert_equal 5, @bst.count
     assert_equal 3, @bst.count(@bst.root.left)
